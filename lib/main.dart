610eaecc9b173e7647ef86602073a8476b566def
Zakariyah Firmansyah <fzakariyah1@gmail.com>
2022-05-03 08:54:46 +0700

    second splash

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class SevenDays extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: FirstSplash(),
+      home: FirstSplash2(),
     );
   }
 }
--- lib/pages/first_splash.dart
+++ lib/pages/first_splash.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class FirstSplash extends StatelessWidget {
-  const FirstSplash({Key? key}) : super(key: key);
+class FirstSplash2 extends StatelessWidget {
+  const FirstSplash2({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0XFF13131E),
+        backgroundColor: const Color(0XFF13131E),
         body: Padding(
           padding: const EdgeInsets.only(top: 230.0, left: 118.0, right: 118.0),
           child: Column(
@@ -18,16 +18,54 @@ class FirstSplash extends StatelessWidget {
                   width: 140,
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 170,
               ),
               Text(
                 "VENTURE",
                 style: GoogleFonts.dmSerifDisplay(
-                    color: Color(0xffFFFFFF), fontSize: 32),
+                    color: const Color(0xffFFFFFF), fontSize: 32),
               )
             ],
           ),
         ));
+  }
+}
+
+class FirstSplash1 extends StatelessWidget {
+  const FirstSplash1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/background.png"),
+                      fit: BoxFit.cover)),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 70.0, left: 77.0, right: 77.0),
+              child: Row(
+                children: [
+                  Image.asset(
+                    "assets/home.png",
+                    width: 51,
+                  ),
+                  Text(
+                    "HouseQu",
+                    style: GoogleFonts.dmSerifDisplay(
+                        fontSize: 32.3, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
